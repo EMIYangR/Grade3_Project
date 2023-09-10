971e37863511121f9619db8b23e1243339f0d22a
EMI <1062738620@qq.com>
2023-09-11 04:03:16 +0800

    前端1.0

---
+++ 01_jw1903_S1 阅博/5、项目源码/YueBo/JS/vue/pl.vue
@@ -0,0 +1,27 @@
+<div class="title">
+    <div class="title-img">
+        <img :src="i.upic" alt="">
+    </div>
+    <div class="title-name">
+        <div class="title-name-top">
+            <span>{{i.name}}</span>
+            <span class="iconfont icon-huangguan"></span>: 
+        </div>
+        <div class="title-name-hour">
+            {{i.ctime}}
+        </div>
+        <div class="title-name-show">
+            来自小米6
+        </div>
+    </div>
+    
+    
+</div>
+<div class="pl">
+    {{i.nr}}
+</div>                                       
+<div class="share1">
+    <div class="iconfont icon-fenxiang"></div>
+    <div class="iconfont icon-pinglun"></div>
+    <div class="iconfont icon-dianzan"></div>
+</div>
